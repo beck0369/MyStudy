4afd5ae30fd4df322ff917c78871ba6d596c6812
beck <0369beck@gmail.com>
2022-01-22 18:05:14 +0900

    0122

---
+++ Study/Assets/Scripts/Chapter4/Chapter4.cs
@@ -0,0 +1,112 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chapter4 : MonoBehaviour
+{
+    //    private void start()
+    //    {
+    //        int a = 111 + 222;
+    //        Debug.Log($"a : {a}");
+
+    //        int b = a - 100;
+    //        Debug.Log("b : " + b);
+
+    //        int c = b * 10;
+    //        Debug.Log($"c : {c}");
+
+    //        float d = c / 6.3f;
+    //        Debug.Log("d : " + d);
+
+    //        Debug.Log($"22 / 7 = {22 / 7}({22 % 7})");
+    //    }   a : 333  b : 233  c : 2330  d : 369.8412  22 / 7 = 3(1)
+
+
+    //private void Start()
+    //{
+    //    int a = 10;
+    //    Debug.Log(a++);
+    //    Debug.Log(++a);
+
+    //    a = 10;
+    //    Debug.Log(a--);
+    //    Debug.Log(--a);
+    //}   10  12  10  8
+
+
+    //private void Start()
+    //{
+    //    int a = 10;
+    //    Debug.Log(a++);
+    //    Debug.Log(++a);
+
+    //    Debug.Log(a--);
+    //    Debug.Log(--a);
+    //}   10  12  12  10
+
+
+    //private void Start()
+    //{
+    //    string result = "123" + "456";
+    //    Debug.Log(result);
+
+    //    result = "Hello" + " " + "World!";
+    //    Debug.Log(result);
+    //}   123456  Hello World!
+
+
+    //private void Start()
+    //{
+    //    Debug.Log($"3 > 4 : {3 > 4}");
+    //    Debug.Log($"3 >= 4 : {3 >= 4}");
+    //    Debug.Log($"3 < 4 : {3 < 4}");
+    //    Debug.Log($"3 <= 4 : {3 <= 4}");
+    //    Debug.Log($"3 == 4 : {3 == 4}");
+    //    Debug.Log($"3 != 4 : {3 != 4}");
+    //}   3 > 4 : False  3 >= 4 : False  3 < 4 : True  3 <= 4 : True  3 == 4 : False  3 != 4 : True
+
+
+    //private void Start()
+    //{
+    //    Debug.Log("Testing && ...");  Testing && ...
+    //    Debug.Log($"1 > 0 && 4 < 5 : {1 > 0 && 4 < 5}");      1 > 0 && 4 < 5 : True
+    //    Debug.Log($"1 > 0 && 4 > 5 : {1 > 0 && 4 > 5}");      1 > 0 && 4 > 5 : False
+    //    Debug.Log($"1 == 0 && 4 > 5 : {1 == 0 && 4 > 5}");    1 == 0 && 4 > 5 : False
+    //    Debug.Log($"1 == 0 && 4 < 5 : {1 == 0 && 4 < 5}");    1 == 0 && 4 < 5 : False
+
+    //    Debug.Log("Testing || ...");  Testing || ...
+    //    Debug.Log($"1 > 0 || 4 < 5 : {1 > 0 || 4 < 5}");      1 > 0 || 4 < 5 : True
+    //    Debug.Log($"1 > 0 || 4 > 5 : {1 > 0 || 4 > 5}");      1 > 0 || 4 > 5 : True
+    //    Debug.Log($"1 == 0 || 4 > 5 : {1 == 0 || 4 > 5}");    1 == 0 || 4 > 5 : False
+    //    Debug.Log($"1 == 0 || 4 < 5 : {1 == 0 || 4 < 5}");    1 == 0 || 4 < 5 : True
+
+    //    Debug.Log("Testing ! ...");       Testing ! ...
+    //    Debug.Log($"!True : {!true}");    !True : False
+    //    Debug.Log($"!False : {!false}");  !False : True
+    //}
+
+
+    //private void Start()
+    //{
+    //    string result = (10 % 2) == 0 ? "Â¦¼ö" : "È¦¼ö";
+    //    Debug.Log(result);    Â¦¼ö
+    //}
+
+
+    //private void Start()
+    //{
+    //    int a;
+    //    a = 100;
+    //    Debug.Log($"a = 100 : {a}");  a = 100 : 100
+    //    a += 90;
+    //    Debug.Log($"a += 90 : {a}");  a += 90 : 190
+    //    a -= 80;
+    //    Debug.Log($"a -= 80 : {a}");  a -= 80 : 110
+    //    a *= 70;
+    //    Debug.Log($"a *= 70 : {a}");  a *= 70 : 7700
+    //    a /= 60;
+    //    Debug.Log($"a /= 60 : {a}");  a /= 60 : 128
+    //    a %= 50;
+    //    Debug.Log($"a %= 50 : {a}");  a %= 50 : 28
+    //}
+}
---
+++ Study/Assets/Scripts/Chapter4/Chapter4_EX1.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chapter4_EX1 : MonoBehaviour
+{
+    // 1. i++와 ++i의 차이점은 무엇인가요?
+    //    답 : i++은 i를 출력 후 값을 더해주는 것이고 ++i는 값을 더해준 후 출력하는 것이다
+
+    // 2. 각 출력문의 값을 적으세요
+    //    int i = 1;
+    //    Debug.Log(i = i + 1); 답 : i = 2
+    //    Debug.Log(i++);       답 : i = 2
+    //    Debug.Log(++i);       답 : i = 4
+    //    Debug.Log(i += 1);    답 : i = 5
+
+    // 3. 다음 코드에서 b는 어떤 값을 가질까요?
+    //    int a =10;
+    //    string b = a == 0 ? "가나다" : "ABC";
+    //    답 : ABC
+}
