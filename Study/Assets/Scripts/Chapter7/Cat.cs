bcc209335561f85e812015459b7583cb287f0b89
beck <0369beck@gmail.com>
2022-01-23 02:01:41 +0900

    0123

---
+++ Study/Assets/Scripts/Chapter7/Cat.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cat : MonoBehaviour
+{
+    //public string name;
+    //public string color;
+
+    //public void Meow()
+    //{
+    //    Debug.Log($"{name} : 야옹");
+    //}
+
+    //public Cat()
+    //{
+    //    Name = "";
+    //    Color = "";
+    //}
+
+    //public Cat(string name, string color)
+    //{
+    //    Name = name;
+    //    Color = color;
+    //}
+
+    //public string Name;
+    //public string Color;
+
+    //Cat kitty = new Cat();
+    //Cat nabi = new Cat("나비", "갈색");
+}
---
+++ Study/Assets/Scripts/Chapter7/Chapter7.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chapter7 : MonoBehaviour
+{
+    //private void Start()
+    //{
+    //    Cat kitty = new Cat();
+    //    kitty.color = "하얀색";
+    //    kitty.name = "키티";
+    //    kitty.Meow();
+    //    Debug.Log($"{kitty.name} : {kitty.color}");
+
+    //    Cat nero = new Cat();
+    //    nero.color = "검은색";
+    //    nero.name = "네로";
+    //    nero.Meow();
+    //    Debug.Log($"{nero.name} : {nero.color}");
+    //}
+
+
+    //private string Name;
+
+    //public void SetName(string Name)
+    //{
+    //    Name = Name;
+    //}
+
+
+    //private string Name;
+
+    //public void SetName(string Name)
+    //{
+    //    this.Name = Name;
+    //}
+
+
+    //int a, b, c;
+
+    //public Chapter7()
+    //{
+    //    this.a = 5425;
+    //}
+
+    //public Chapter7(int b)
+    //{
+    //    this.a = 5425;
+    //    this.b = b;
+    //}
+
+    //public Chapter7(int b, int c)
+    //{
+    //    this.a = 5425;
+    //    this.b = b;
+    //    this.c = c;
+    //}
+}
