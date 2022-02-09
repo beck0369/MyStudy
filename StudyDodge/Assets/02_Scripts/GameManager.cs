2d47188520a5147f775a9950d878a9d547bd2b07
Jinhyeon-Kim <0369beck@gmail.com>
2022-02-09 17:53:10 +0900

    0209

--- StudyDodge/Assets/02_Scripts/GameManager.cs
+++ StudyDodge/Assets/02_Scripts/GameManager.cs
@@ -6,5 +6,48 @@ using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
+    public GameObject gameOverText; // 게임오버 시 활성화할 텍스트 게임 오브젝트
+    public Text timeText;   // 생존 시간을 표시할 텍스트 컴포넌트
+    public Text recordText; // 최고 기록을 표시할 텍스트 컴포넌트
 
+    private float surviveTime;  // 생존 시간
+    private bool isGameover;    // 게임오버 상태
+
+    private void Start()
+    {
+        surviveTime = 0;
+        isGameover = false; // 생존 시간과 게임오버 상태 초기화
+    }
+
+    private void Update()
+    {
+        if (!isGameover)    // 게임오버가 아닌 동안
+        {
+            surviveTime += Time.deltaTime;  // 생존 시간 갱신
+            timeText.text = "Time : " + (int)surviveTime;   // 갱신한 생존 시간을 timeText 텍스트 컴포넌트를 이용해 표시
+        }
+        else
+        {
+            if (Input.GetKeyDown(KeyCode.R))
+            {
+                SceneManager.LoadScene("SampleScene");
+            }
+        }
+    }
+
+    public void EndGame()
+    {
+        isGameover = true;  // 현재 상태를 게임오버 상태로 전환
+        gameOverText.SetActive(true);   // 게임오버 텍스트 게임 오브젝트를 활성화
+
+        float bestTime = PlayerPrefs.GetFloat("BestTime");  // BestTime 키로 저장된 이전까지의 최고 기록 가져오기
+
+        if (surviveTime > bestTime) // 이전까지의 최고 기록보다 현재 생존 시간이 더 크다면
+        {
+            bestTime = surviveTime; // 최고 기록 값을 현재 생존 시간 값으로 변경
+            PlayerPrefs.SetFloat("BestTime", bestTime); // 변경된 최고 기록을 BestTime 키로 저장
+        }
+
+        recordText.text = "Best Time : " + (int)bestTime;   // 최고 기록을 recordText 텍스트 컴포넌트를 이용해 표시
+    }
 }
