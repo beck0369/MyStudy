5ebd1f938b951c265475186c8812dbb87fb38275
Jinhyeon-Kim <0369beck@gmail.com>
2022-04-19 12:38:58 +0900

    20220419

---
+++ HomeWorkUnity/Temp/StagingArea/Il2Cpp/il2cppOutput/Assembly-CSharp_CodeGen.c
@@ -0,0 +1,171 @@
+﻿#include "pch-c.h"
+#ifndef _MSC_VER
+# include <alloca.h>
+#else
+# include <malloc.h>
+#endif
+
+
+#include "codegen/il2cpp-codegen-metadata.h"
+
+
+
+
+
+// 0x00000001 System.Void BackGroundLoop::Awake()
+extern void BackGroundLoop_Awake_m7724BE4D64C38862B1E5471C99F29150C8B0E621 (void);
+// 0x00000002 System.Void BackGroundLoop::Update()
+extern void BackGroundLoop_Update_m8F52F02CA88FE02241665A1F0B5DF8EFE5516832 (void);
+// 0x00000003 System.Void BackGroundLoop::Reposition()
+extern void BackGroundLoop_Reposition_m73476C5FC3DEBBF5A9F363439A8CF0AC6DEA3852 (void);
+// 0x00000004 System.Void BackGroundLoop::.ctor()
+extern void BackGroundLoop__ctor_m45F02E389929F074A10E05D896230D15CBD116C6 (void);
+// 0x00000005 System.Void GameManager::Awake()
+extern void GameManager_Awake_m22F42B2A82708B10F652CAD8F2E0A4767110FF30 (void);
+// 0x00000006 System.Void GameManager::OnEnable()
+extern void GameManager_OnEnable_mD5C3947351399CD9D94EAABE360192091328B2E2 (void);
+// 0x00000007 System.Void GameManager::AddScore(System.Int32)
+extern void GameManager_AddScore_mF7D9082C1C297FC0547D0DDE027410E708CEA226 (void);
+// 0x00000008 System.Void GameManager::OnPlayerDead()
+extern void GameManager_OnPlayerDead_m8453D356730F5F6C43D5542061DF2F81BC074109 (void);
+// 0x00000009 System.Boolean GameManager::Damage()
+extern void GameManager_Damage_m2D54449195EA8707695725AC886294A9482D8309 (void);
+// 0x0000000A System.Void GameManager::.ctor()
+extern void GameManager__ctor_mE8666F6D0CA9C31E16B719F79780DC4B0245B64D (void);
+// 0x0000000B System.Void GoldFish::OnEnable()
+extern void GoldFish_OnEnable_mE892401209C71B53D89CB58078377FAE3010596B (void);
+// 0x0000000C System.Void GoldFish::.ctor()
+extern void GoldFish__ctor_m5BCA03C17C80EEB03C40815295AA90D145A0821D (void);
+// 0x0000000D System.Void Platform::OnEnable()
+extern void Platform_OnEnable_m968CEFADB0D0C4C1749DF925212A6C131F0411F1 (void);
+// 0x0000000E System.Void Platform::OnCollisionEnter2D(UnityEngine.Collision2D)
+extern void Platform_OnCollisionEnter2D_m0DDB610BA92333877F1D2C1B95AF4B5CC278BD24 (void);
+// 0x0000000F System.Void Platform::.ctor()
+extern void Platform__ctor_m639396BBA3F91A7EEC3B3CB6C1C7D5D71494433C (void);
+// 0x00000010 System.Void PlatformSpawner::Start()
+extern void PlatformSpawner_Start_m6759F98C407F783258EC7F9234E6F660FFD1E98C (void);
+// 0x00000011 System.Void PlatformSpawner::Update()
+extern void PlatformSpawner_Update_mB8D6E237AF1B75B8A56E2B7951C324A4CAC44F41 (void);
+// 0x00000012 System.Void PlatformSpawner::.ctor()
+extern void PlatformSpawner__ctor_m400EDCFDB08DEFAB0C61C9A84C960FC6BC221219 (void);
+// 0x00000013 System.Void PlayerController::Start()
+extern void PlayerController_Start_m9531F30EC892BDD1758A2EEC724E86EFBDA150A3 (void);
+// 0x00000014 System.Void PlayerController::Update()
+extern void PlayerController_Update_mB31159CAD7DD2329859472554BC9154A83D8E794 (void);
+// 0x00000015 System.Void PlayerController::Die()
+extern void PlayerController_Die_m2A44CB9CC6A92B0963A0F5D3FA8B6A95F46EA533 (void);
+// 0x00000016 System.Void PlayerController::OnTriggerEnter2D(UnityEngine.Collider2D)
+extern void PlayerController_OnTriggerEnter2D_m1B1E3F94F29560C6CD4687C6556D74A092CC672E (void);
+// 0x00000017 System.Void PlayerController::OnCollisionEnter2D(UnityEngine.Collision2D)
+extern void PlayerController_OnCollisionEnter2D_m9A113567D03E9BFF74D8D1959B8C88124E03BBDE (void);
+// 0x00000018 System.Void PlayerController::OnCollisionExit2D(UnityEngine.Collision2D)
+extern void PlayerController_OnCollisionExit2D_m1006B66A7BB5A396E0C5238BD46CC3E2DEACE27F (void);
+// 0x00000019 System.Void PlayerController::.ctor()
+extern void PlayerController__ctor_mF30385729DAFDFCB895C4939F6051DCE6C0327FB (void);
+// 0x0000001A System.Void Powerpole::OnEnable()
+extern void Powerpole_OnEnable_m2CDED0B89400FD7F684072A83273ACE60EF9FF15 (void);
+// 0x0000001B System.Void Powerpole::.ctor()
+extern void Powerpole__ctor_mC04CF0E416EEDEED664C8AB6B90740C7CAC32E9E (void);
+// 0x0000001C System.Void Scene::Restart()
+extern void Scene_Restart_mA89F60BD0B86FFFD7CE52C57AFCF53AE500E82D3 (void);
+// 0x0000001D System.Void Scene::Exit()
+extern void Scene_Exit_mF8D850B7A68E7901CE946D7955BB5B7F7B24F386 (void);
+// 0x0000001E System.Void Scene::.ctor()
+extern void Scene__ctor_mE4E08911D71ED67675EDD133FDA16063AF953EA9 (void);
+// 0x0000001F System.Void ScrollingObject::Update()
+extern void ScrollingObject_Update_m066B3CC0507818CD8BEEE1604E803BD2C4D38F77 (void);
+// 0x00000020 System.Void ScrollingObject::.ctor()
+extern void ScrollingObject__ctor_m2853459981CCFBF0B248346062B3B767D20250FF (void);
+static Il2CppMethodPointer s_methodPointers[32] = 
+{
+	BackGroundLoop_Awake_m7724BE4D64C38862B1E5471C99F29150C8B0E621,
+	BackGroundLoop_Update_m8F52F02CA88FE02241665A1F0B5DF8EFE5516832,
+	BackGroundLoop_Reposition_m73476C5FC3DEBBF5A9F363439A8CF0AC6DEA3852,
+	BackGroundLoop__ctor_m45F02E389929F074A10E05D896230D15CBD116C6,
+	GameManager_Awake_m22F42B2A82708B10F652CAD8F2E0A4767110FF30,
+	GameManager_OnEnable_mD5C3947351399CD9D94EAABE360192091328B2E2,
+	GameManager_AddScore_mF7D9082C1C297FC0547D0DDE027410E708CEA226,
+	GameManager_OnPlayerDead_m8453D356730F5F6C43D5542061DF2F81BC074109,
+	GameManager_Damage_m2D54449195EA8707695725AC886294A9482D8309,
+	GameManager__ctor_mE8666F6D0CA9C31E16B719F79780DC4B0245B64D,
+	GoldFish_OnEnable_mE892401209C71B53D89CB58078377FAE3010596B,
+	GoldFish__ctor_m5BCA03C17C80EEB03C40815295AA90D145A0821D,
+	Platform_OnEnable_m968CEFADB0D0C4C1749DF925212A6C131F0411F1,
+	Platform_OnCollisionEnter2D_m0DDB610BA92333877F1D2C1B95AF4B5CC278BD24,
+	Platform__ctor_m639396BBA3F91A7EEC3B3CB6C1C7D5D71494433C,
+	PlatformSpawner_Start_m6759F98C407F783258EC7F9234E6F660FFD1E98C,
+	PlatformSpawner_Update_mB8D6E237AF1B75B8A56E2B7951C324A4CAC44F41,
+	PlatformSpawner__ctor_m400EDCFDB08DEFAB0C61C9A84C960FC6BC221219,
+	PlayerController_Start_m9531F30EC892BDD1758A2EEC724E86EFBDA150A3,
+	PlayerController_Update_mB31159CAD7DD2329859472554BC9154A83D8E794,
+	PlayerController_Die_m2A44CB9CC6A92B0963A0F5D3FA8B6A95F46EA533,
+	PlayerController_OnTriggerEnter2D_m1B1E3F94F29560C6CD4687C6556D74A092CC672E,
+	PlayerController_OnCollisionEnter2D_m9A113567D03E9BFF74D8D1959B8C88124E03BBDE,
+	PlayerController_OnCollisionExit2D_m1006B66A7BB5A396E0C5238BD46CC3E2DEACE27F,
+	PlayerController__ctor_mF30385729DAFDFCB895C4939F6051DCE6C0327FB,
+	Powerpole_OnEnable_m2CDED0B89400FD7F684072A83273ACE60EF9FF15,
+	Powerpole__ctor_mC04CF0E416EEDEED664C8AB6B90740C7CAC32E9E,
+	Scene_Restart_mA89F60BD0B86FFFD7CE52C57AFCF53AE500E82D3,
+	Scene_Exit_mF8D850B7A68E7901CE946D7955BB5B7F7B24F386,
+	Scene__ctor_mE4E08911D71ED67675EDD133FDA16063AF953EA9,
+	ScrollingObject_Update_m066B3CC0507818CD8BEEE1604E803BD2C4D38F77,
+	ScrollingObject__ctor_m2853459981CCFBF0B248346062B3B767D20250FF,
+};
+static const int32_t s_InvokerIndices[32] = 
+{
+	1123,
+	1123,
+	1123,
+	1123,
+	1123,
+	1123,
+	954,
+	1123,
+	1111,
+	1123,
+	1123,
+	1123,
+	1123,
+	963,
+	1123,
+	1123,
+	1123,
+	1123,
+	1123,
+	1123,
+	1123,
+	963,
+	963,
+	963,
+	1123,
+	1123,
+	1123,
+	1123,
+	1123,
+	1123,
+	1123,
+	1123,
+};
+extern const CustomAttributesCacheGenerator g_AssemblyU2DCSharp_AttributeGenerators[];
+IL2CPP_EXTERN_C const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule;
+const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule = 
+{
+	"Assembly-CSharp.dll",
+	32,
+	s_methodPointers,
+	0,
+	NULL,
+	s_InvokerIndices,
+	0,
+	NULL,
+	0,
+	NULL,
+	0,
+	NULL,
+	NULL,
+	g_AssemblyU2DCSharp_AttributeGenerators,
+	NULL, // module initializer,
+	NULL,
+	NULL,
+	NULL,
+};
